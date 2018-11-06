ce4da0d39651a470e0524e279082393d627789f2
James Stauder <James.Stauder@umontana.edu>
2018-11-06 15:52:35 -0700

    finished canvas UI for game

--- pythonTyper/pythonGameScript.js
+++ pythonTyper/pythonGameScript.js
@@ -1,9 +1,20 @@
 var startGame = false;
 
-var canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext('2d');
-var windowWidth = canvas.width;
-var windowHeight = canvas.height;
+var mainCanvasWidthPercent = .8;
+var mainCanvasHeightPercent = .94;
+var mainCanvas = document.getElementById('mainCanvas');
+
+var scoreCanvasWidthPercent = .8;
+var scoreCanvasHeightPercent = .05;
+var scoreCanvas = document.getElementById('scoreCanvas');
+
+var consoleCanvasWidthPercent = .19;
+var consoleCanvasHeightPercent = 1;
+var consoleCanvas = document.getElementById('consoleCanvas');
+
+ctxMain = mainCanvas.getContext('2d');
+ctxScore = scoreCanvas.getContext('2d');
+ctxConsole = consoleCanvas.getContext('2d');
 
 $(document).ready(function($){
     $('button').on('click', function() {
@@ -20,18 +31,34 @@ function windowResize(){
 }
 
 function resizeCanvas(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+
+    scoreCanvas.width = window.innerWidth * scoreCanvasWidthPercent;
+    scoreCanvas.height = window.innerHeight * scoreCanvasHeightPercent;
+
+    mainCanvas.width = window.innerWidth * mainCanvasWidthPercent;
+    mainCanvas.height = window.innerHeight * mainCanvasHeightPercent;
+
+    consoleCanvas.width = window.innerWidth * consoleCanvasWidthPercent;
+    consoleCanvas.height = window.innerHeight * consoleCanvasHeightPercent;
+    
     draw();
 }
 function draw()
-{
-    //This is outlining the canvas. 
-    ctx.strokeStyle = 'blue';
-    ctx.lineWidth = '5';
-    ctx.strokeRect(0,0, window.innerWidth, window.innerHeight);
+{ 
+    ctxMain.strokeStyle = 'blue';
+    ctxMain.lineWidth = '5';
+    ctxMain.strokeRect(0,0, mainCanvas.width, mainCanvas.height);
+    ctxMain.fillText("This is the main Canvas", 10,50);
 
+    ctxScore.strokeStyle = 'red';
+    ctxScore.lineWidth = '5';
+    ctxScore.strokeRect(0,0, scoreCanvas.width, scoreCanvas.height);
+    ctxScore.fillText("This is the Scoreboard Canvas", 10,50);
 
+    ctxConsole.strokeStyle ='orange';
+    ctxConsole.lineWidth ='5';
+    ctxConsole.strokeRect(0,0,consoleCanvas.width, consoleCanvas.height);
+    ctxConsole.fillText("This is the console Canvas", 10,50);
 
 }
 
